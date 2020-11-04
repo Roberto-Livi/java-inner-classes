90b2b2b78a3db0e173fa79dc680010edbe8d0d22
Roberto-Livi <robertomlivi@outlook.com>
2020-11-03 23:53:10 -0500

    applied a local and anonymous class inside the Main method

--- inner-classes/src/com/company/Main.java
+++ inner-classes/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     private static Scanner scanner = new Scanner(System.in);
@@ -7,17 +9,41 @@ public class Main {
 
     public static void main(String[] args) {
 
-        class ClickListener implements Button.OnClickListener {
-            public ClickListener(){
-                System.out.println("I've been activated");
-            }
+        // Local Class
+//        class ClickListener implements Button.OnClickListener {
+//            public ClickListener(){
+//                System.out.println("I've been activated");
+//            }
+//
+//            @Override
+//            public void onClick(String title) {
+//                System.out.println(title + " was clicked");
+//            }
+//        }
+//        btnPrint.setOnClickListener(new ClickListener());
 
+        // Anonymous Class
+        btnPrint.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(String title) {
                 System.out.println(title + " was clicked");
             }
+        });
+    }
+
+    private static void listen() {
+        boolean quit = false;
+        while(!quit) {
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch(choice) {
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    btnPrint.onClick();
+            }
         }
-        btnPrint.setOnClickListener(new ClickListener());
     }
 
 }
